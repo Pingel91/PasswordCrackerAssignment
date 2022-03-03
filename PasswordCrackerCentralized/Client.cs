8191476f643fe0c7b820718aecb81306d8f743a1
Usama Ahmad <usam0034@edu.easj.dk>
2022-03-03 11:00:04 +0100

    TCP setup

---
+++ PasswordCrackerCentralized/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerCentralized
+{
+    class Client
+    {
+        private const int Port = 7777;
+        public Client()
+        {
+
+        }
+
+        public void start()
+        {
+            TcpClient Client = new TcpClient("LocalHost", Port);
+            using (StreamWriter sw = new StreamWriter(Client.GetStream()))
+            {
+                
+                
+                sw.AutoFlush = true;
+                string Hello = "Hello";
+                //string helloString = 
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ PasswordCrackerCentralized/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCrackerCentralized
+{
+    class Server
+    {
+        private const int Port = 7777;
+        public Server()
+        {
+            
+        }
+
+        public void start()
+        {
+            TcpListener listener = new TcpListener(Port);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task.Run(
+                    () =>
+                    {
+                        TcpClient tmpSocket = socket;
+                        //DoClient(tmpSocket);
+                    }
+                );
+            }
+        }
+
+        private void DoClient(TcpClient socket)
+        {
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+               
+            }
+            socket?.Close();
+        }
+
+
+    }
+}
